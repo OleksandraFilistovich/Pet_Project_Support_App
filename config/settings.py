408874bdc6dc339a8effefd7a25f359dd839aa7d
Oleksandra Filistovich <alexandrafilistovich@gmail.com>
2023-06-26 00:55:32 +0300

    :memo: Lesson 19 code implemented

--- config/settings.py
+++ config/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    # "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
@@ -126,3 +126,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 POKEAPI_BASE_URL = "https://pokeapi.co/api/v2/pokemon"
+
+AUTH_USER_MODEL = "core.User"
