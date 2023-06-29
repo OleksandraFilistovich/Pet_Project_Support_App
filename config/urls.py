a1bf7f954f8ef6fe25b41a6ac0aaea36e16e2474
Oleksandra Filistovich <alexandrafilistovich@gmail.com>
2023-06-29 22:12:32 +0300

    :sparkles: code quality

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 from core.api import UserRegistrationAPIView
-from examples.hw_serializers import test_serializers
 
 urlpatterns = [
     path("admin/", admin.site.urls),
