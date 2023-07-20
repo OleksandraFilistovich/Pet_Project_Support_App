7a5a19ebaa7731658a4c175da435803e747a4a5a
Oleksandra Filistovich <alexandrafilistovich@gmail.com>
2023-07-20 15:01:06 +0300

    :bug: Bug with .env fixed. Deleted useless from standart gitignore.

--- src/config/settings.py
+++ src/config/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from datetime import timedelta
 from distutils.util import strtobool
 from os import getenv
-from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = getenv("DJANGO_SECRET_KEY", default="^nwuz631@d_ez()m2qb!a1@!dx!l_(zl(u7p)pyw673wq0!bf2")
+SECRET_KEY = getenv("DJANGO_SECRET_KEY", default="invalid")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = strtobool(getenv("DJANGO_DEBUG", default="true"))
+DEBUG = strtobool(getenv("DJANGO_DEBUG", default="false"))
 
 ALLOWED_HOSTS = ["*"]
 
@@ -142,7 +142,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-POKEAPI_BASE_URL = getenv("POKEAPI_BASE_URL", default="https://pokeapi.co/api/v2/pokemon")
+POKEAPI_BASE_URL = getenv("POKEAPI_BASE_URL", default="invalid")
 
 
 AUTH_USER_MODEL = "users.User"
@@ -172,11 +172,11 @@ if DEBUG is True:
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(
-        seconds=getenv("JWT_ACCESS_TOKEN_LIFETIME", default=15),
+        seconds=int(getenv("JWT_ACCESS_TOKEN_LIFETIME", default=25)),
     ),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
 
 
-CELERY_BROKER_URL = getenv("CELERY_BROKER_URL", default="redis://broker:6379/0")
+CELERY_BROKER_URL = getenv("CELERY_BROKER_URL", default="invalid")
